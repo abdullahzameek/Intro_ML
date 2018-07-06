1ddbf7b6bd870ba8a848e0aeafddabc22e10a678
abdullahzameek <35721712+abdullahzameek@users.noreply.github.com>
2018-07-06 19:52:57 +0530

    Started on my own Pytho3 version

---
+++ src/networkpy3.py
@@ -0,0 +1,16 @@
+import random
+import numpy as np
+
+class Network:
+    def __init__(self, sizes):
+        self.num_layer = len(sizes)
+        self.sizes = sizes #represents number of neurons in each layer
+        self.biases = [np.random.randn(y,1) for y in sizes[1:]] #initialise the biases randomly with Gaussian distribution of mean 0 and standard deviation 1
+        self.weights = [np.random.randn(y,x) 
+                for x,y in zip(sizes[:-1], sizes[1:])] #ditto as above
+#The code assumes that the first layer is an input layer, and so it 
+# doesnt assign any weights or biases to it
+
+def sigmoid(z):
+    return 1.0(1.0+np.exp(-z))
+    
